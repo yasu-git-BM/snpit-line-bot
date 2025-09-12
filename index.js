71959321a872ef12c92048c593d4cf76b1427ce9
Yasu <bends_kurt@yahoo.co.jp>
2025-09-12 20:50:52 +0900

    エラー対応

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const statusRouter = require('./api/status');
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
 const client = new Client(config);
